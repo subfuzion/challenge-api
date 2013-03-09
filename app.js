7ef06c0e575a2f58d394d128e51b34ba48249898
Tony Pujals <tony@subfuzion.com>
2013-03-09 01:35:10 -0800

    Initial commit.
    Can retrieve XML feed from official challenges URI and return as JSON.

---
+++ app.js
@@ -0,0 +1,109 @@
+// ==================================================================
+// After connecting to mongo start listening for requests
+// ==================================================================
+
+var mongo = require('mongodb');
+var express = require('express');
+var http = require('http');
+var xml2js = require('xml2js');
+
+var app = express();
+app.use(express.bodyParser());
+
+var db = new mongo.Db('challengedb',
+	new mongo.Server('ds033047.mongolab.com', 33047, {auto_reconnect: true}),
+	{w: 'majority'});
+
+db.open(function(err, client) {
+	if (err) {
+		console.log('error opening db: ' + err);
+	} else {
+		db.addListener("error", function(error){
+			console.log("db error: " + error);
+		});
+
+		client.authenticate('api-user', 'challengedb', function(err, success) {
+			if (err) {
+				console.log('error authenticating with database');
+			} else {
+				console.log('authenticated with database');
+				var port = 8080;
+				app.listen(port, function () {
+					console.log('listening on ' + port);
+				});
+			}
+		});
+	}
+});
+
+
+// ==================================================================
+// Routes
+// ==================================================================
+
+app.get('/', function(req, res, next) {
+});
+
+
+app.get('/feed', function(req, res, next) {
+	getFeed(function(err, json) {
+		if (err) {
+			return next(err);
+		} else {
+			res.end(JSON.stringify(json));
+		}
+	});
+});
+
+
+app.get('/harvest', function(req, res, next) {
+	getFeed(function(err, json) {
+		if (err) {
+			return next(err);
+		} else {
+			res.end(JSON.stringify(json));
+		}
+	});
+
+});
+
+
+// ==================================================================
+// Implementation
+// ==================================================================
+
+/**
+ * Returns the latest government challenges as a javascript object.
+ * @param callback
+ */
+function getFeed(callback)
+{
+	var options = {
+		host: 'challenge.gov',
+		path: '/api/challenges.xml'
+	};
+
+	http.request(options, function(resp) {
+		var xml = '';
+
+		resp.on('data', function(chunk) {
+			xml += chunk;
+		});
+
+		resp.on('end', function() {
+			xml2js.parseString(xml, function(err, js) {
+				if (err) {
+					return callback(err);
+				} else {
+					callback(null, js);
+				}
+			});
+		})
+	}).end();
+};
+
+
+function saveChallenges(callback) {
+
+};
+
